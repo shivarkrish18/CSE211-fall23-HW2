8f7c45cb2dc47b51a97694e0a609ab3aab971305
Vinh <vihle@ucsc.edu>
2023-11-01 14:26:05 -0700

    Added some to part1

--- part1/skeleton.py
+++ part1/skeleton.py
@@ -1,4 +1,5 @@
-import argparse 
+import argparse
+import re
 
 def local_value_numbering(f):
     f = open(f)
@@ -10,8 +11,69 @@ def local_value_numbering(f):
     to_optimize = s.split("// Start optimization range")[1].split("// End optimization range")[0]
 
     # hint: perform the local value numbering optimization here on to_optimize
+
+    Current_val = dict()
+    H = dict()
+    counter = 4
+    lines = to_optimize.split("\n")
+    new_lines = []
+    statments = []
+    for line in lines:
+        if '=' in line:
+            new_lines.append(line.lstrip().rstrip(';'))
+    for i in range(len(new_lines)):
+        statement = ''
+        var1 = new_lines[i][0]
+        var2 = new_lines[i][4]
+        var3 = new_lines[i][8]
+        operator = new_lines[i][6]
+        if(i==0):
+            statement = var1 + '3' + ' = ' + var2 + '1' + ' ' + operator + ' ' + var3 + '2'
+            Current_val[var1] = 3
+            Current_val[var2] = 1
+            Current_val[var3] = 2
+            H[var2 + '1' + ' ' + operator + ' ' + var3 + '2'] = [var1 + '3']
+        else:
+            print('Here ', Current_val)
+            if var2 not in Current_val.keys():
+                Current_val[var2] = counter
+                counter+=1
+            if var3 not in Current_val.keys():
+                Current_val[var3] = counter
+                counter+=1
+            Current_val[var1] = counter
+            counter+=1
+            if operator == '+':
+                index1 = var2 + str(Current_val[var2]) +' + ' + var3 + str(Current_val[var3])
+                index2 = var3 + str(Current_val[var3]) +' + '+ var2 + str(Current_val[var2])
+                if index1 in H.keys():
+                    statement = var1 + str(Current_val[var1]) + ' = ' + H[index1][-1]
+                    H[index1].append(var1+str(Current_val[var1]))
+                elif index2 in H.keys():
+                    statement = var1 + str(Current_val[var1]) + ' = ' + H[index2][-1]
+                    H[index2].append(var1+str(Current_val[var1]))
+                else:
+                    statement = var1 + str(Current_val[var1]) + ' = ' + var2 + str(Current_val[var2]) +' + ' + var3 + str(Current_val[var3])
+                    H[statement.split(' = ')[1]] = [var1 + str(Current_val[var1])]
+            else:
+                index1 = var2 + str(Current_val[var2]) +' - ' + var3 + str(Current_val[var3])
+                if index1 in H.keys():
+                    statement = var1 + str(Current_val[var1]) + ' = ' + H[index1][-1]
+                    H[index1].append(var1+str(Current_val[var1]))
+
+                else:
+                    statement = var1 + str(Current_val[var1]) + ' = ' + var2 + str(Current_val[var2]) +' - ' + var3 + str(Current_val[var3])
+                    H[statement.split(' = ')[1]] = [var1 + str(Current_val[var1])]
+        statments.append(statement)
+
+
+    print('Statements ',statments)
+    print()
+    print('Hashtable ', H)
+    print()
+    print('Current Values ', Current_val)
     
-    print(pre)
+    #print(pre)
 
     # hint: print out any new variable declarations you need here
 
@@ -19,7 +81,7 @@ def local_value_numbering(f):
 
     # hint: store any new numbered variables back to their unumbered counterparts here
     
-    print(post)
+    #print(post)
 
     # You should keep track of how many instructions you replaced
     #print("// replaced: " + str(replaced))    
